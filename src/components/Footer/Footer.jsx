14b212e160559272e3c7ed5d741c8464739c63d3
Your Name <you@example.com>
2023-10-20 11:01:27 +0530

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './footer.css';
+
+const Footer = () => {
+  return (
+		<div className="footer">
+			Made with 💚 by
+			<a href="https://dugarrishab.github.io/RishabDugar/"> Rishab </a>
+      and 
+      <a href=""> Aanya</a>
+		</div>
+  );
+}
+
+export default Footer
